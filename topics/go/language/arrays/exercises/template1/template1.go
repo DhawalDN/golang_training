18e9e21ea35d1aa1dd2aedf4f4defd854bcfb306
Dhawal Dyavanpalli <dhawaldyavanpalli@gmail.com>
2019-08-23 08:50:23 +0530

    Array Completed

--- topics/go/language/arrays/exercises/template1/template1.go
+++ topics/go/language/arrays/exercises/template1/template1.go
@@ -1,5 +1,3 @@
-
-
 // Declare an array of 5 strings with each element initialized to its zero value.
 //
 // Declare a second array of 5 strings and initialize this array with literal string
@@ -7,16 +5,25 @@
 // Display the string value and address of each element.
 package main
 
+import "fmt"
+
 // Add imports.
 
 func main() {
 
 	// Declare an array of 5 strings set to its zero value.
-
+	var persons [5]string
 	// Declare an array of 5 strings and pre-populate it with names.
+	users := [5]string{"A", "B", "C", "D", "E"}
 
 	// Assign the populated array to the array of zero values.
-
+	persons[1] = users[1]
+	for i, user := range users {
+		persons[i] = user
+	}
 	// Iterate over the first array declared.
-	// Display the string value and address of each element.
+	for i := range persons {
+		// Display the string value and address of each element.
+		fmt.Printf("Persons[%d]= '%v' || address = %p\n", i, persons[i], &persons[i])
+	}
 }
