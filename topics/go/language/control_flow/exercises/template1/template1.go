382dcb001f53ba97bf5fd77b3c3f4890685f4014
Dhawal Dyavanpalli <dhawaldyavanpalli@gmail.com>
2019-08-23 08:50:51 +0530

    Control Flow

--- topics/go/language/control_flow/exercises/template1/template1.go
+++ topics/go/language/control_flow/exercises/template1/template1.go
@@ -1,5 +1,3 @@
-
-
 // Write a program that inspects a user's name and greets them in a certain way
 // if they are on a list or in a different way if they are not. Also look at
 // the user's age and tell them some special secret if they are old enough to
@@ -7,14 +5,30 @@
 package main
 
 // Add imports
+import (
+	"fmt"
+)
 
 func main() {
 
 	// Change these values and rerun your program.
-	name := "Carter"
-	age := 6
+	name := "Moe"
+	age := 22
 
 	// If the user's name is on a special list then give them a secret greeting.
+	switch name {
+	case "John", "Bella", "Jack", "Moe":
+		fmt.Println("You are a hero")
+	}
 
 	// If the user is old enough then tell them a secret.
+	switch {
+	case age > 18:
+		{
+			fmt.Println("You are eligible")
+		}
+	default:
+		fmt.Println("Can't tell you any secret")
+	}
+
 }
